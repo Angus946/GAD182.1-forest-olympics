bdec01593c1fd43db316cab37be899a187a973dc
Angus <1034241@student.sae.edu.au>
2025-11-09 20:54:13 +1000

    BeetlefulHarvest By Lachlan

---
+++ Assets/Enemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{// code in green is for later with UI
+ // main main;
+    Transform tr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+  //      main = GameObject.Find("scripts").GetComponent<main>();
+    }
+
+
+    void FixedUpdate()
+    {
+        tr.position -= new Vector3(0f, 0.12f, 0f);
+        if (tr.position.y < -7f) Destroy(this.gameObject);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+         //   main.GameOver = true;
+        }
+    }
+
+
+
+
+}
