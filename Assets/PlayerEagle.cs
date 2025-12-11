91d96dfa5220f840a9e4d97aad9eecd903572f5c
SAE1\1034241 <1034241@student.sae.edu.au>
2025-12-11 10:51:15 +1000

    pulling and pushing changes

---
+++ Assets/PlayerEagle.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerEagle : MonoBehaviour
+{
+    public GameManager gameManager;
+    public bool isDead = false;
+    public float velocity = 2.4f;
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.collider.tag == "Ground")
+        {
+            isDead = true;
+            gameManager.GameOver();
+        }
+        
+    }
+}
+
+
+
+
